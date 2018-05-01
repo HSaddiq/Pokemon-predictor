6dec9033d9cd8823fa78d10befc117d84772c0ea
Hannan <saddiqhb@gmail.com>
2018-05-01 08:37:22 +0100

    Added predict function in predictor.py using pretrained model

--- predictor.py
+++ predictor.py
@@ -1,75 +1,51 @@
 import pandas as pd
 import tensorflow as tf
 import numpy as np
+from type_multiplier import find_type_multiplier
+import data_parser as data_parser
 
-'''x_test = np.array([[1,100,110,90,85,90,60,4,95,109,105,75,85,56]])
-x_test = pd.DataFrame(x_test)
-x_test = x_test.as_matrix()
-print(x_test)
 
-feed_dict = {
-    x: x_test
-    }
-print(sess.run(y, feed_dict))'''
+def generate_input_data(pokemon_name_1, pokemon_name_2):
+    pokemon_matrix = data_parser.generate_pokemon_matrix()
+
+    for i in range(pokemon_matrix.shape[0]):
+        # check if name is one of chosen values
+        if pokemon_matrix[i][1] == pokemon_name_1:
+            pokemon_1_data = list(pokemon_matrix[i])
+            pokemon_1_data = pokemon_1_data[2:-2]
+
+        elif pokemon_matrix[i][1] == pokemon_name_2:
+            pokemon_2_data = list(pokemon_matrix[i])
+            pokemon_2_data = pokemon_2_data[2:-2]
+
+    pokemon_1_types = [type for type in pokemon_1_data[:2] if type != ""]
+    pokemon_2_types = [type for type in pokemon_2_data[:2] if type != ""]
+
+    output = [find_type_multiplier(pokemon_1_types,
+                                   pokemon_2_types)] + pokemon_1_data[
+                                                       2:] + [
+                 find_type_multiplier(
+                     pokemon_2_types, pokemon_1_types)] + pokemon_2_data[2:]
+
+    return np.array([output])
 
 
 def predict(pokemon_name_1, pokemon_name_2):
-    #initialising variables
-
-    input_nodes = 14
-
-    hidden_nodes1 = 10
-    hidden_nodes2 = 20
-
-    pkeep = tf.placeholder(tf.float32)
-
-    # input
-    x = tf.placeholder(tf.float32, [None, input_nodes])
-
-    # layer 1
-    W1 = tf.Variable(
-        tf.truncated_normal([input_nodes, hidden_nodes1], stddev=0.15))
-    b1 = tf.Variable(tf.zeros([hidden_nodes1]))
-    y1 = tf.nn.relu(tf.matmul(x, W1) + b1)
-
-    # layer 2
-    W2 = tf.Variable(
-        tf.truncated_normal([hidden_nodes1, hidden_nodes2], stddev=0.15))
-    b2 = tf.Variable(tf.zeros([hidden_nodes2]))
-    y2 = tf.nn.relu(tf.matmul(y1, W2) + b2)
-
-    # layer 3
-    W3 = tf.Variable(tf.truncated_normal([hidden_nodes2, 1], stddev=0.15))
-    b3 = tf.Variable(tf.zeros([1]))
-    y3 = tf.nn.sigmoid(tf.matmul(y2, W3) + b3)
-
-    # output
-    y = y3
-    y_ = tf.placeholder(tf.float32, [None, 1])
-
     # initialising tensorflow
-    with tf.Session() as sess:
-        # import previously generated model
-        saver = tf.train.import_meta_graph(
-            "./trained_model/trained_predictor.meta")
-        saver.restore(sess, tf.train.latest_checkpoint("./trained_model/."))
+    # import previously generated model
+    saver = tf.train.import_meta_graph(
+        "./trained_model/trained_predictor.meta")
 
-        #initialise variables and graph
-        sess.run(tf.global_variables_initializer())
+    sess = tf.Session()
 
-        graph = tf.get_default_graph()
+    saver.restore(sess, "./trained_model/trained_predictor")
 
-        x_test = np.array(
-            [[1, 100, 110, 90, 85, 90, 60, 4, 95, 109, 105, 75, 85, 56]])
-        x_test = pd.DataFrame(x_test)
-        x_test = x_test.as_matrix()
+    # generate input data line using names of pokemon
+    input = generate_input_data(pokemon_name_1, pokemon_name_2)
 
-        feed_dict = {
-            x: x_test
-        }
+    input = pd.DataFrame(input)
+    input = input.as_matrix()
 
-        prediction = sess.run(y, feed_dict)
+    prediction = sess.run("y3:0", feed_dict={"x:0": input})
 
-        return prediction
-
-print(predict(1,1))
+    return prediction
